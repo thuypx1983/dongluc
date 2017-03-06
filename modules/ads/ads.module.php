cd5244ddfe4e3d31d69649c9b6de1c7593afaac2
Pham Xuan Thuy <phamxuanthuy@sutunam.com>
2017-03-06 10:33:45 +0700

    fancybox image

--- modules/ads/ads.module.php
+++ modules/ads/ads.module.php
@@ -78,23 +78,8 @@ class ads extends base {
 
 
     function youtube_id_from_url($url) {
-        $pattern =
-            '%^# Match any youtube URL
-                (?:https?://)?  # Optional scheme. Either http or https
-                (?:www\.)?      # Optional www subdomain
-                (?:             # Group host alternatives
-                  youtu\.be/    # Either youtu.be,
-                | youtube\.com  # or youtube.com
-                  (?:           # Group path alternatives
-                    /embed/     # Either /embed/
-                  | /v/         # or /v/
-                  | /watch\?v=  # or /watch\?v=
-                  )             # End path alternatives.
-                )               # End host alternatives.
-                ([\w-]{10,12})  # Allow 10-12 for 11 char youtube id.
-                $%x'
-        ;
-        $result = preg_match($pattern, $url, $matches);
+        $result=preg_match("/^(?:http(?:s)?:\/\/)?(?:www\.)?(?:m\.)?(?:youtu\.be\/|youtube\.com\/(?:(?:watch)?\?(?:.*&)?v(?:i)?=|(?:embed|v|vi|user)\/))([^\?&\"'>]+)/", $url, $matches);
+
         if ($result) {
             return $matches[1];
         }
