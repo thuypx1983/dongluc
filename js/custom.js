973a4f2321a0f35ebdd6d0421a1534184e99bf1c
dongluc.dev.oms.vn <dongluc_dev_oms_vn@packer-debian-8-amd64.droplet.local>
2017-03-06 02:22:17 +0000

    custom.js

--- js/custom.js
+++ js/custom.js
@@ -1,4 +1,28 @@
-$(function(){
-    //stuff
-    //viet code tai day
-})(jQuery)
+$(document).ready(function(){
+	$('.news-category ul.product-slider-right').slick({
+		infinite: true,
+		slidesToShow: 3,
+		slidesToScroll: 1,
+		autoplay: true,
+		vertical: true,
+		autoplaySpeed: 2000,
+		nextArrow: '<i class="fa fa-chevron-down custom-slick-arr slick-arr-down"></i>',
+        prevArrow: '<i class="fa fa-chevron-up custom-slick-arr slick-arr-up"></i>',
+	});
+	$('.news-service ul.news-items').slick({
+		infinite: true,
+		slidesToShow: 4,
+		slidesToScroll: 1,
+		autoplay: true,
+		autoplaySpeed: 2000,
+		nextArrow: '<i class="fa fa-chevron-right custom-slick-arr slick-arr-right"></i>',
+        prevArrow: '<i class="fa fa-chevron-left custom-slick-arr slick-arr-left"></i>',
+	});
+	$('.videos ul.video-items').slick({
+		infinite: true,
+		slidesToShow: 3,
+		slidesToScroll: 1,
+		nextArrow: '<i class="fa fa-chevron-right custom-slick-arr slick-arr-right"></i>',
+        prevArrow: '<i class="fa fa-chevron-left custom-slick-arr slick-arr-left"></i>',
+	});
+});
