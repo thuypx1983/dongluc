f0f90e968ecddffba20c08d466a24457080a524d
PHAM XUAN THUY <thuypx1983@gmail.com>
2017-04-23 11:11:51 +0700

    video module

---
+++ modules/video/templates/showList.tpl
@@ -0,0 +1,75 @@
+<div class="grid1060">
+    <div class="content_cate pkg">
+      
+      <div class="col70per fr">
+        <!-- <a href="{$url}dich-vu/" class="head_title">Dịch vụ</a> -->
+
+        <div class="breadcumb_product pkg">
+          <div class="fl">
+            <a href="{$url}dich-vu/" class="first_bread">
+                Dịch vụ{if count($bread)>0}<span class="arr_bread"><img src="{$url}images/arr_brad.png"/></span>{/if}
+            </a>
+            {foreach from=$bread item=bre key=k}
+            <a href="{$url}{if $bre.pre_link}{$bre.pre_link}-{$bre.id}/{/if}">{$bre.title}</a>
+            {if count($bread)>($k+1)}<span class="arr_bread"><img src="{$url}images/arr_brad.png"/></span>{/if}
+                
+            {/foreach}
+            </div>
+        </div>
+
+        <ol id="blog-list" class="products-list" style="margin-top:20px">
+            {foreach from=$rows item=row key=k}
+                <li class="postWrapper item">
+                    <div class="item-inner">
+                      <div class="product-info row">
+                        <div class="col-lg-3" style="padding-left:0"> <a href="{$url}{$row.pre_link}-{$row.id}.html"><img src="{$url}upload/news/thumb/{$row.photo|default:'no-img.jpg'}" class="img_list_news"></a> </div>
+                        <div class="col-lg-9">
+                          <div class="product-name">
+                            <h2><a class="title-blog-name" href="{$url}{$row.pre_link}-{$row.id}.html">{$row.title}</a></h2>
+                            <!-- <h3>3/6/2014 12:58 AM</h3> -->
+                            <div class="info-cat-blog">{$row.create_date}</div>
+                            <p  class="desc_just">{$row.description|truncate:400}</p>
+                          </div>
+                        </div>
+                        <div class="clear"></div>
+                        {if ($k+1) < count($rows)}
+                        <div class="postContent ">
+                          <div class="bottom-info-blog"> </div>
+                        </div>
+                        {/if}
+                      </div>
+                    </div>
+                  </li>
+            {foreachelse}
+                <div style="margin-left:0px; padding:10px 0 20px;">
+                    <em><p>Chưa cập nhật nội dung</p></em>
+                </div>
+            {/foreach}
+          
+        </ol>
+        <div class="paging">
+            {$paging}
+        </div>
+      </div>
+      <div class="col30per fl">
+        {loadModule mod=news task=category}
+        {loadModule mod=news task=list_top}
+        {*loadModule mod=product task=list_top*}
+        {loadModule mod=ads task=banner_sidebar}
+
+      </div>
+    </div>
+  </div>
+
+<div class="popular-product">
+    <div class="block-title">
+        <span>Sản phẩm nổi bật</span>
+    </div>
+    {loadModule mod=product task=list_top_home}
+</div>
+<div class="partner">
+    <div class="block-title">
+        <span>Đối tác</span>
+    </div>
+    {loadModule mod=ads task=partner}
+</div>
---
+++ modules/video/video.module.php
@@ -0,0 +1,238 @@
+<?php
+class news extends base
+{
+    var $table = "ads";
+
+    function run($task, $param = "")
+    {
+        if ($_SESSION['username'] == 'root') {
+            #echo '<pre>'; print_r($_SERVER); echo '</pre>';
+            #echo '<pre>'; print_r($_GET); echo '</pre>';
+        }
+        $this->table = "ads";
+        switch ($task) {
+            case "detail":
+                $this->detail();
+                break;
+        case "list":
+                $this->detail();
+                break;
+
+        }
+    }
+
+
+    function detail()
+    {
+
+
+        $sql = "
+			SELECT *,
+			concat('{$this->pre}', '/', link) as pre_link
+			FROM {$this->table}
+			{$this->where} AND id = '{$_GET['nid']}'
+		";
+        $row = $this->db->getRow($sql);
+
+        $row["content"] = stripslashes($row["content"]);
+        $row["title"] = stripslashes($row["title"]);
+        $row["content"] = str_replace("upload/editor/", SITE_URL . "upload/editor/", $row["content"]);
+        $this->smarty->assign("row", $row);
+
+        //bread
+        $_GET['id'] = $row['news_category_id'];
+        if ($_GET['nid'] != '') {
+            $_GET['menu_type'] = 'dich-vu';
+            $bread = array();
+            $temp = $this->db->getRow("select id, title, link, parent_id, concat('{$_GET['menu_type']}', '/', link) as pre_link from {$this->table_category} {$this->where} AND id = '{$_GET['id']}' ");
+            $bread[] = $temp;
+            if ($temp['parent_id'] > 0) {
+                $temp = $this->db->getRow("select id, title, link, parent_id, concat('{$_GET['menu_type']}', '/', link) as pre_link from {$this->table_category} {$this->where} AND id = '{$temp['parent_id']}' ");
+                $bread[] = $temp;
+                if ($temp['parent_id'] > 0) {
+                    $temp = $this->db->getRow("select id, title, link, parent_id, concat('{$_GET['menu_type']}', '/', link) as pre_link from {$this->table_category} {$this->where} AND id = '{$temp['parent_id']}' ");
+                    $bread[] = $temp;
+                    if ($temp['parent_id'] > 0) {
+                        $temp = $this->db->getRow("select id, title, link, parent_id, concat('{$_GET['menu_type']}', '/', link) as pre_link from {$this->table_category} {$this->where} AND id = '{$temp['parent_id']}' ");
+                        $bread[] = $temp;
+                    }
+                }
+            }
+        }
+        #echo '<pre>'; print_r($bread); echo '</pre>';
+        $this->smarty->assign("bread", array_reverse($bread));
+
+        // lấy bài liên quan
+        $where = " AND id != '{$row['id']}' ";
+
+        $sql = "
+			SELECT id, title, concat('{$this->pre}', '/', link) as pre_link, photo, description
+			FROM {$this->table}
+			{$this->where} {$where} 
+		";
+
+        $arr1 = $this->db->getAll($sql . " AND id < {$row['id']} ORDER BY id DESC LIMIT 2");
+        $arr2 = $this->db->getAll($sql . " AND id > {$row['id']} ORDER BY id ASC LIMIT 2");
+
+        $c_arr1 = count($arr1);
+        $c_arr2 = count($arr2);
+
+        if ($c_arr1 < 2) {
+            $arr2 = $this->db->getAll($sql . " AND id > {$row['id']} ORDER BY id ASC LIMIT " . (4 - $c_arr1));
+        }
+
+        if ($c_arr2 < 2) {
+            $arr1 = $this->db->getAll($sql . " AND id < {$row['id']} ORDER BY id DESC LIMIT " . (4 - $c_arr2));
+        }
+        krsort($arr1);
+        $rows = array_merge($arr1, $arr2);
+
+        #$rows = $this->db->getAll($sql);
+
+        foreach ($rows as $k => $v) {
+            $v["content"] = stripslashes($v["content"]);
+            $rows[$k]["content"] = stripslashes($v["content"]);
+            $rows[$k]["title"] = stripslashes($v["title"]);
+            //$rows[$k]["photo"] = str_replace("upload/editor/", SITE_URL."upload/editor/", $this->getSrcImg($v['content']));
+        }
+        #echo "<pre>";print_r($rows);echo "</pre>";
+        $this->smarty->assign("rows", $rows);
+
+        $this->smarty->display("detail.tpl");
+    }
+
+    function showList()
+    {
+        if ($_GET['id'] != '') {
+            $_GET['menu_type'] = 'dich-vu';
+            // breadcrumb
+            $bread = array();
+            $temp = $this->db->getRow("select id, title, link, parent_id, concat('{$_GET['menu_type']}', '/', link) as pre_link from {$this->table_category} where id = '{$_GET['id']}' ");
+            $bread[] = $temp;
+            if ($temp['parent_id'] > 0) {
+                $temp = $this->db->getRow("select id, title, link, parent_id, concat('{$_GET['menu_type']}', '/', link) as pre_link from {$this->table_category} where id = '{$temp['parent_id']}' ");
+                $bread[] = $temp;
+                if ($temp['parent_id'] > 0) {
+                    $temp = $this->db->getRow("select id, title, link, parent_id, concat('{$_GET['menu_type']}', '/', link) as pre_link from {$this->table_category} where id = '{$temp['parent_id']}' ");
+                    $bread[] = $temp;
+                    if ($temp['parent_id'] > 0) {
+                        $temp = $this->db->getRow("select id, title, link, parent_id, concat('{$_GET['menu_type']}', '/', link) as pre_link from {$this->table_category} where id = '{$temp['parent_id']}' ");
+                        $bread[] = $temp;
+                        if ($temp['parent_id'] > 0) {
+                            $temp = $this->db->getRow("select id, title, link, parent_id, concat('{$_GET['menu_type']}', '/', link) as pre_link from {$this->table_category} where id = '{$temp['parent_id']}' ");
+                            $bread[] = $temp;
+                        }
+                    }
+                }
+            }
+            $this->smarty->assign("bread", array_reverse($bread));
+
+        }
+
+        $where = "";
+        if ($_GET['id'] != '') {
+            /*$where = " AND news_category_id = '{$_GET['id']}' ";*/
+            $where .= " AND ( news_category_id = '{$_GET['id']}' OR news_category_id IN (SELECT id FROM {$this->table_category} WHERE parent_id = '{$_GET['id']}' ) OR news_category_id IN (SELECT id FROM {$this->table_category} WHERE parent_id IN (SELECT id FROM {$this->table_category} where parent_id = '{$_GET['id']}' ) ) ) ";
+            $category = $_GET['cate'] . "-" . $_GET['id'] . "/";
+        } else {
+            $where .= " AND news_category_id!=18 ";
+        }
+        // phuc vu cho paging
+        $page = isset($_GET['page']) ? $_GET['page'] : 1;
+        $limit = 8;
+        $start = $limit * ($page - 1);
+
+        $sql = "
+			SELECT *,
+			concat('{$this->pre}', '/', link) as pre_link
+			FROM {$this->table}
+			{$this->where} $where
+			ORDER BY `create_date` DESC
+			LIMIT $start, $limit
+		";
+        $rows = $this->db->getAll($sql);
+        #echo "<pre>";print_r($rows);echo "</pre>";
+        echo mysql_error();
+        $this->smarty->assign("rows", $rows);
+
+        $text_link_page = ($_SESSION["lang"] == "vi") ? "trang" : "page";
+        $action_path = SITE_URL . $this->pre . '/' . $category . "{$text_link_page}-{i}/";
+
+        $numrows = $this->db->getOne("SELECT count(id) FROM {$this->table} {$this->where} $where ");
+        $oPaging = new Paging($numrows, $limit, Null, "page");
+        $oPaging->set_current_page($page);
+        $oPaging->set_action_path($action_path);
+        $oPaging->set_site_url(SITE_URL);
+
+        if ($numrows > $limit) {
+            $this->smarty->assign("paging", $oPaging->string_paging());
+        }
+        $this->smarty->assign("numrows", $numrows);
+        $this->smarty->display('showList.tpl');
+
+        return;
+        $title = $this->db->getOne("SELECT title FROM {$this->table_type} WHERE link = '{$_GET['menu_type']}' ");
+        $this->smarty->assign("row_title", $title);
+
+        $title_sub = $this->db->getOne("SELECT title FROM {$this->table_category} WHERE id = '{$_GET['id']}' ");
+        $this->smarty->assign("row_title_sub", $title_sub);
+
+        $where = " AND menu_type = '{$_GET['menu_type']}' ";
+        $where .= " AND news_category_id = '{$_GET['id']}' ";
+
+        $cateinfo = $this->db->getRow("SELECT id, title, link, page_template, concat('{$this->pre}', '/', link) as pre_link FROM {$this->table_category} WHERE id = '{$_GET['id']}' ");
+        $cate_link = "/" . $cateinfo["link"] . "-" . $cateinfo["id"];
+
+        if ($cateinfo['page_template'] != '') {
+
+            $this->page_template($cateinfo['page_template']);
+
+            return;
+        }
+
+        // phuc vu cho paging
+        $page = isset($_GET['page']) ? $_GET['page'] : 1;
+        $limit = 8;
+        $start = $limit * ($page - 1);
+
+        $sql = "
+			SELECT *,
+			concat(menu_type, '/', link) as pre_link
+			FROM {$this->table} 
+			{$this->where} $where
+			{$this->order}
+			LIMIT $start, $limit
+		";
+        $rows = $this->db->getAll($sql);
+        #echo "<pre>";print_r($rows);echo "</pre>";
+        echo mysql_error();
+
+        foreach ($rows as $k => $v) {
+            $rows[$k]["title"] = stripslashes($v["title"]);
+            //$rows[$k]["content"] = stripslashes($v["content"]);
+            //$rows[$k]["photo"] = str_replace("upload/editor/", SITE_URL."upload/editor/", $this->getSrcImg($v['content']));
+        }
+
+        $this->smarty->assign("rows", $rows);
+
+        $text_link_page = ($_SESSION["lang"] == "vi") ? "trang" : "page";
+
+        $action_path = SITE_URL . $_GET['menu_type'] . $cate_link . $search_link . "/{$text_link_page}-{i}/";
+
+        $numrows = $this->db->getOne("SELECT count(id) FROM {$this->table} {$this->where} $where ");
+        $oPaging = new Paging($numrows, $limit, Null, "page");
+        $oPaging->set_current_page($page);
+        $oPaging->set_action_path($action_path);
+        $oPaging->set_site_url(SITE_URL);
+
+        if ($numrows > $limit) {
+            $this->smarty->assign("paging", $oPaging->string_paging());
+        }
+        $this->smarty->assign("numrows", $numrows);
+
+        $this->smarty->display('showList.tpl');
+
+    }
+}
+
+?>
